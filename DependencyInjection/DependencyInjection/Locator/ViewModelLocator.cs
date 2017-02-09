6ec1b610d298fa508725922a45958b04333bdcc3
mapaux <josevi.agullo@gmail.com>
2017-02-09 10:00:26 +0000

    Added ViewModelLocator

---
+++ DependencyInjection/DependencyInjection/Locator/ViewModelLocator.cs
@@ -0,0 +1,30 @@
+﻿using DependencyInjection.Services;
+using DependencyInjection.ViewModels;
+using Microsoft.Practices.ServiceLocation;
+using Microsoft.Practices.Unity;
+
+namespace DependencyInjection.Locator
+{
+    public class ViewModelLocator
+    {
+        private UnityContainer _unityContainer;
+
+        public ViewModelLocator()
+        {
+            _unityContainer = new UnityContainer();
+
+            // Register Services here
+            _unityContainer.RegisterType<IEmployeeService, EmployeeService>();
+
+            // Register ViewModels here
+            _unityContainer.RegisterType<MainViewModel>(new ContainerControlledLifetimeManager());
+
+            var unityServiceLocator = new UnityServiceLocator(_unityContainer);
+
+            ServiceLocator.SetLocatorProvider(() => unityServiceLocator);
+        }
+
+        // Create method foreach ViewModel
+        public MainViewModel MainViewModel => _unityContainer.Resolve<MainViewModel>();
+    }
+}
